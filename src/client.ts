66632babf17e79774cb3242d2e880c78f807497f
Stafford Brunk <stafford@guildeducation.com>
2019-06-25 11:23:52 -0600

    Add DEFAULT_SCOPE support to Auth0 client + override ability

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,21 @@
 import { ManagementClient, ManagementClientOptions } from 'auth0'
 
+import { Auth0Flags } from './flags/auth0'
+
+const DEFAULT_SCOPE =
+  'read:tenant_settings update:tenant_settings read:clients update:clients'
+
 export function buildManagementClient(
+  flags: Auth0Flags,
   options?: ManagementClientOptions
 ): ManagementClient {
   // Only scope to the permissions we need to do page updates by default
   // Login page is updated on the client. Other pages are tenent settings
-  let cleanOptions = options || {
-    domain: process.env.AUTH0_DOMAIN!,
-    clientId: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    scope:
-      'read:tenant_settings update:tenant_settings read:clients update:clients'
+  const buildOptions: ManagementClientOptions = {
+    domain: '',
+    scope: DEFAULT_SCOPE,
+    ...flags,
+    ...options
   }
-  return new ManagementClient(cleanOptions)
+  return new ManagementClient(buildOptions)
 }
